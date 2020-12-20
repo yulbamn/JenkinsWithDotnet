e3e46ac5c0992f43798fec180c9e963ad3ad7f82
MinGyu, Ju <knighp@gmail.com>
2020-12-20 15:29:11 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace JenkinsWithDotnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Min ^^ !");
+                    await context.Response.WriteAsync("Hello MinGyu ^^ !");
                 });
             });
         }
